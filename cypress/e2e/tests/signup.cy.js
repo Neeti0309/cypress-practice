b0678cba54e2435efd9e1021f425fae1f2123891
Neeti Sharma <sharma.niti03@gmail.com>
2025-05-09 15:41:14 +0530

    signup login page code added

---
+++ cypress/e2e/tests/signup.cy.js
@@ -0,0 +1,19 @@
+describe('User Signup flow for automationexcrcise website', () => {
+  beforeEach(() =>{
+    cy.visit('https://automationexercise.com/login')
+  });
+
+ it('should allow a user to sign up successfully', () => {
+    // Fill in the signup form
+   cy.get('[data-qa="signup-name"]').type('neeti mishra')
+   cy.get('[data-qa="signup-email"]').type('sharma.niti03@gmail.com');
+
+    //Submit the form
+  cy.get('[data-qa="signup-button"]').click();
+
+    //assertion after signup
+     cy.url().should('include', '/signup');
+     // cy.contain('Enter Account Informations').should('be.visible');
+   });
+
+  })
